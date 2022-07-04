a151998abbc6be8ebf245005a3b6ff4026e162e7
Raul Roldan <raul.ji@outlook.com>
2022-07-04 13:00:16 -0500

    Finished js

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -52,4 +52,5 @@ console.log(me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
-
+me.favoriteColor = 'Green';
+console.log(me.favoriteColor);
